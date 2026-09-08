a4d539be37b12256e802351d1d71f8e71e4c589d
abhi-r21 <connerbrain14@gmail.com>
2026-09-08 21:19:15 +0530

    Fixed the video issue

--- Portfolio/src/components/Hero.tsx
+++ Portfolio/src/components/Hero.tsx
@@ -5,6 +5,9 @@ function Hero() {
       <video src="/videos/Video Project 5.mp4" autoPlay loop muted playsInline></video>
       <img src="/toji.jpeg" alt="Profile" className="profile-pic"/>
       </div>
+      <div>
+      <h1 className="name">Abhi Ram</h1>
+      </div>
     </section>
   );
 }
