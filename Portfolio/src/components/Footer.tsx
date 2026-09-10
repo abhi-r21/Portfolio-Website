ec6c3cdba9e3c41262cb4cdedd6196ac4e4995f5
abhi-r21 <connerbrain14@gmail.com>
2026-09-10 10:31:57 +0530

    Added few changes to the Hero section

---
+++ Portfolio/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return(
+    <section className="footer">
+
+    </section>
+  );
+}
+
+export default Footer;
--- Portfolio/src/components/Hero.tsx
+++ Portfolio/src/components/Hero.tsx
@@ -1,12 +1,29 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa"; 
+import { SiX } from "react-icons/si";
+
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-video">
       <video src="/videos/Video Project 5.mp4" autoPlay loop muted playsInline></video>
-      <img src="/toji.jpeg" alt="Profile" className="profile-pic"/>
       </div>
-      <div>
-      <h1 className="name">Abhi Ram</h1>
+
+      <img src="/toji.jpeg" alt="Profile" className="profile-pic"/>
+
+      <div className="portfolio-info">
+          <h1 className="name">Abhiram Bojja</h1>
+          
+          <div className="social-icons">
+            <a href="http://github.com/abhi-r21" target="_blank">
+            <FaGithub />
+            </a>
+            <a href="https://x.com/Ab_hiii21" target="_blank">
+            <SiX/>
+            </a>
+            <a href="https://www.linkedin.com/in/abhiram-bojja/" target="_blank">
+            <FaLinkedin/>
+            </a>
+          </div>
       </div>
     </section>
   );
