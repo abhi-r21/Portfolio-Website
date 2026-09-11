707f57e54a20ca3ce9ff9dde706ca6bfc59f6c61
abhi-r21 <connerbrain14@gmail.com>
2026-09-11 13:25:38 +0530

    Added Changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,8 +10,8 @@ function Navbar() {
           <MusicPlayer />
         </div>
         <a href="#home" className="transition-colors duration-300 hover:text-gray-400">Home</a>
-        <a href="#about" className="transition-colors duration-300 hover:text-gray-400">About</a>
         <a href="#projects" className="transition-colors duration-300 hover:text-gray-400">Projects</a>
+        <a href="#about" className="transition-colors duration-300 hover:text-gray-400">About</a>
         <a href="#contact" className="transition-colors duration-300 hover:text-gray-400">Contact</a>
 
       </div>
