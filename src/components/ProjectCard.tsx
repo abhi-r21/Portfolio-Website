253f32a8c4d636ab75cdb8adb024fef74a18195e
abhi-r21 <connerbrain14@gmail.com>
2026-09-11 12:32:31 +0530

    Added effects to the card components

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,19 +1,19 @@
 function ProjectCard() {
   return(
-    <div className="w-[445px] rounded-2xl border border-white/20 bg-[#151515] p-4
-    transition-all duration-300 ease-out hover: -translate-y-2 hover:border-white/40 hover: shadow-xl"
+    <div className="group w-[445px] rounded-2xl border border-white/20 bg-[#151515] p-4
+    transition-all duration-300 ease-out hover: -translate-y-2 hover:scale-[1.02] hover:border-white/50 hover: shadow-xl hover:shadow-[0_0_30px_rgba(255,255,255,0.08)]"
     >
 
         {/* Project image */}
         <div className="h-64 w-full overflow-hidden rounded-2xl">
           <img src="/images/Ethereal Purple Canyon Landscape.jpg" alt="Project-1"
-           className="h-full w-full object-cover transition-transform duration-500 ease-out hover:scale-105"/>
+           className="h-full w-full object-cover blur-[1px] transition-transform duration-500 ease-out hover:scale-110 hover: brightness-110"/>
         </div>
         
         {/* Project information */}
         <div className="mt-5 flex items-start justify-between">
           <div>
-            <h3 className="text-2xl font-semibold">
+            <h3 className="text-2xl font-semibold transition-transform duration-300 group-hover:translate-x-1">
               Project 1
             </h3>
             
