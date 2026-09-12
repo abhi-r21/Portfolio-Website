b67e1caac2ef8981aba7f5d8eb7b232673e89f2f
abhi-r21 <connerbrain14@gmail.com>
2026-09-12 10:39:22 +0530

    Fixed the Issue in AboutMe section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,20 +8,27 @@ function AboutMe() {
     target: ref,
     offset: ["start start", "end start"]
   });
+  
+  //Move About Me from lower position to the top
+  const y = useTransform(scrollYProgress, [0, 0.5], [500, 0]);
 
-  const y = useTransform(scrollYProgress, [0,1], [250,0]);
-  const scale = useTransform(scrollYProgress, [0,1], [1.1, 1]);
+  //Slightly reduce its size while moving
+  const scale = useTransform(scrollYProgress, [0, 0.35], [1.1, 1]);
   
   return (
-    <section id="aboutme"
+    <section 
+    id="aboutme"
     ref={ref}
-    className="relative h-[150vh]"
+    className="relative h-[200vh]"
     >
+      {/* This is the position where the heading will stay */}
+    <div className="sticky top-8 z-20">
       <motion.h1
       style={{y, scale}}
-      className="top-8 z-10 mt-10 text-center font-[Cormorant_Garamond] text-5xl font-bold">
+      className="text-center font-[Cormorant_Garamond] text-5xl font-bold">
         About Me
       </motion.h1>
+    </div>
 
       <AboutText />
     </section>
--- src/components/AboutText.tsx
+++ src/components/AboutText.tsx
@@ -2,35 +2,41 @@ import { motion, useScroll, useTransform } from "motion/react";
 import { useRef } from "react";
 
 function AboutText() {
-  const textRef =useRef<HTMLDivElement>(null);
+  const ref =useRef<HTMLDivElement>(null);
 
   const{ scrollYProgress } = useScroll({
-    target: textRef,
+    target: ref,
     offset: ["start center", "end center"]
   });
+  
   const opacity = useTransform(
     scrollYProgress, [0, 0.4, 0.8, 1], [0, 1, 1, 0]
-  );
-
-  const y = useTransform(
-    scrollYProgress, [0,1], [100, -100]
   )
 
+  const blur = useTransform(
+    scrollYProgress, [0, 0.6, 1], [0, 0, 8]
+  );
+
+  const filter = useTransform(
+    blur, 
+    (value) => `blur(${value}px)`
+  );
+
   return (
-    <section 
-    ref={textRef} 
+    <div 
+    ref={ref} 
     className="flex min-h-screen items-center justify-center px-6"
     >
-      <motion.div
-      style = {{ opacity, y }}
-      className="max-w-5xl text-center text-2xl leading-relaxed"
+      <motion.p
+      style = {{ opacity, filter }}
+      className="max-w-5xl text-center text-2xl leading-relaxed text-gray-400"
       >
         I'm a developer who enjoys turning ideas into clean, functional
         digital experiences. I like experimenting with new technologies,
         building projects, and constantly finding better ways to solve
         problems.
-      </motion.div>
-    </section>
+      </motion.p>
+    </div>
   )
 }
 
